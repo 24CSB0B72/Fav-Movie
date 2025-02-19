ab2d39c84c1e76d56b9db88d8a8c93f1d5fe6c4c
24CSB0B72 <sm24csb0b72@student.nitw.ac.in>
2025-02-19 11:47:17 +0530

    added another feature

--- script.js
+++ script.js
@@ -4,14 +4,28 @@ let updateMovie = () => {
     document.querySelector(".page-title").innerText = `FAV MOVIE - ${Movies.length}`;
     let area = document.querySelector(".movieArea");
     area.innerHTML = '';
-    Movies.forEach(todo => {
+    Movies.forEach((todo, index) => {
         movieElement = document.createElement("div");
         movieElement.classList.add("todo-element");
         movieElement.innerText = todo["movie-name"];
+        
+        // Add click event to remove movie on click
+        movieElement.addEventListener("click", () => {
+            removeMovie(index); // Call the removeMovie function with the index of the movie
+        });
+
         area.appendChild(movieElement);
     });
 }
+
+let removeMovie = (index) => {
+    // Remove movie from Movies array based on index
+    Movies.splice(index, 1);
+    updateMovie(); // Re-render the updated movie list
+};
+
 document.addEventListener("DOMContentLoaded", updateMovie);
+
 let addMovie = () => {
     let currentMovie = document.querySelector("#movie-input").value;
     if (currentMovie.trim() !== "") {
@@ -19,16 +33,18 @@ let addMovie = () => {
             "movie-name": currentMovie,
         };
         Movies.push(movieObject);
-        
     }
 };
+
 let removeAllMovies = () => {
     Movies = [];
     let area = document.querySelector(".movieArea");
     area.innerHTML = '';
     document.querySelector(".page-title").innerText = `FAV MOVIE - ${Movies.length}`;
-}
+};
+
 document.querySelector(".remove-all-btn").addEventListener("click", removeAllMovies);
+
 document.querySelector(".submitbtn").addEventListener("click", () => {
     let movieInput = document.querySelector("#movie-input");
     if (movieInput.value === "") {
